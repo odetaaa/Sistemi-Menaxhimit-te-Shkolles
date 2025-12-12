d1b7056b72c1396c61881937d821c70057e080db
greta0304 <Greta.Rika@fri.edu.al>
2025-12-12 21:23:44 +0100

    U shtuan klasat Nota.java dhe Mungesa.java

---
+++ src/projekt/Mungesa.java
@@ -0,0 +1,14 @@
+package projekt;
+
+import java.util.Date;
+
+public class Mungesa {
+
+    private int id;
+    private Nxenesi nxenesi;
+    private Lenda lenda;
+    private Date data;
+    private boolean eArsyetuar;
+
+    public Mungesa() {}
+}
---
+++ src/projekt/Nota.java
@@ -0,0 +1,11 @@
+package projekt;
+
+public class Nota {
+
+    private int id;
+    private Nxenesi nxenesi;
+    private Lenda lenda;
+    private int nota;
+
+    public Nota() {}
+}
