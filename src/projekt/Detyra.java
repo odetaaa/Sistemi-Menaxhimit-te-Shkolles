0ebc80794aeeed9e800c8ca69fe331a22011dfe5
serenamile1 <Serena.Mile@fti.edu.al>
2025-12-12 20:56:10 +0100

    Shtova klasat Detyra.java dhe VleresimiDetyres.java

---
+++ src/projekt/Detyra.java
@@ -0,0 +1,16 @@
+package projekt;
+
+import java.util.Date;
+
+public class Detyra {
+
+    private int id;
+    private String titulli;
+    private Date afatiDorezimit;
+    private Mesuesi krijuarNga;
+    private Lenda lenda;
+
+    public Detyra() {}
+}
+
+
---
+++ src/projekt/VleresimiDetyres.java
@@ -0,0 +1,12 @@
+package projekt;
+
+public class VleresimiDetyres {
+
+    private int id;
+    private Nxenesi nxenesi;
+    private Detyra detyra;
+    private String skedariDorezuar;
+    private int nota;
+
+    public VleresimiDetyres() {}
+}
