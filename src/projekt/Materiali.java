a58971caa32ff6b0c4c88d3cca81167c86a9408b
fabjola111 <mujafabjola@gmail.com>
2025-12-12 21:40:37 +0100

    U shtua klasa Materiali.java

---
+++ src/projekt/Materiali.java
@@ -0,0 +1,12 @@
+package projekt;
+
+public class Materiali {
+
+    private int id;
+    private String titulli;
+    private String skedari;
+    private Mesuesi ngarkuarNga;
+    private Lenda lenda;
+
+    public Materiali() {}
+}
